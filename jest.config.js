2b17a6469ea10086f831a4ac750e5de2d4f118c0
Jacob Hummel <j4cobhummel@gmail.com>
2022-05-16 22:48:12 -0500

    chore: add jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: "react-native",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  collectCoverageFrom: ["./src/**"],
+  coverageThreshold: {
+    global: {
+      branches: 20, // TODO: set to 70%+
+    },
+  },
 };
